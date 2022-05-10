2770dab5e1dd9aeaa4cc529fe965383bd778eb3f
kuuube <hexagonisalie@gmail.com>
2022-05-10 10:46:54 -0400

    Feat: Check if items can be added to cart

--- check_artisan_stock.py
+++ check_artisan_stock.py
@@ -63,6 +63,7 @@ from datetime import datetime,timezone
 #red: color 1
 
 request_url = "https://www.artisan-jp.com/get_syouhin.php"
+cart_url = "https://www.artisan-jp.com/stock_recheck.php"
 
 #when adding a new mousepad model it must be added into the models dictionary
 #new pads will likely fit existing hardness, size, and colors so those may not need to be edited
@@ -102,11 +103,37 @@ dict_colors = {
 
 utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
 
-in_stock_list = []
+in_cart_list = []
+only_stock_list = []
+only_cart_list = []
+cart = False
 
+def check_cart(stock_check):
+    try:
+        split_request = stock_check.split("/")
+        split_request[1] = split_request[1] + " " + split_request[2]
+        split_request[2] = "1"
+        del split_request[5]
+        del split_request[5]
+        combined_request = ",".join(split_request)
+        
+        cookies = {"cart":combined_request, "disc": "1", "lung": "jpf", "souryou": "800,SAL"}
+        add_to_cart = requests.post(cart_url, cookies=cookies)
+        if combined_request == add_to_cart.text:
+            cart = True
+        else:
+            cart = False
+        return cart
+    except Exception as e:
+        print("Cart check failed:")
+        print(e)
+        cart = False
+        return cart
+    
 def stock_checker(request_data):
     for item in itertools.product (*request_data):
         data = {
+            "kuni": "on",
             "sir": item[0] + item[1],
             "size": item[2],
             "color": item[3]
@@ -114,27 +141,39 @@ def stock_checker(request_data):
         try:
             stock_check = requests.post(request_url, data)
             stock_regex = re.search("^[0-z]+(?=\/)",stock_check.text)
-            if stock_regex == None:
-                stock_message = "In stock: True, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
-                in_stock_list.append(dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]])
-                print(stock_message)
-            elif stock_regex.group(0) != "NON":
-                stock_message = "In stock: True, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
-                in_stock_list.append(dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]])
-                print(stock_message)
+            
+            cart = check_cart(stock_check.text)
+            
+            #in stock
+            if stock_regex == None or stock_regex.group(0) != "NON":
+                if cart == True:
+                    stock_message = "Stock check: True, Cart check: True, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
+                    in_cart_list.append(dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]])
+                    print(stock_message)
+                else:
+                    stock_message = "Stock check: True, Cart check: False, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
+                    only_stock_list.append(dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]])
+                    print(stock_message)
+            #out of stock
             else:
-                stock_message = "In stock: False, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
-                print(stock_message)
+                if cart == True:
+                    stock_message = "Stock check: False, Cart check: True, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
+                    only_cart_list.append(dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]])
+                    print(stock_message)
+                else:
+                    stock_message = "Stock check: False, Cart check: False, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
+                    print(stock_message)
+                
         except Exception as e:
-            print("Request failed\n")
+            print("Request failed:")
             print(e)
-            stock_message = "In stock: Request failed, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
+            stock_message = "Stock check: Request failed, Model: " + dict_mousepad_models[item[0]] + ", Hardness: " + dict_hardnesses[item[1]] + ", Size: " + dict_sizes[item[2]] + ", Color: " + dict_colors[item[3]]
         try:
             with open ("artisan_stock_record_" + utc_time + ".txt", "a") as stock_record:
                 stock_record.write(stock_message)
                 stock_record.write("\n")
         except Exception as e:
-            print("Could not open or write to file\n")
+            print("Could not open or write to file:")
             print(e)
             input()
             
@@ -213,12 +252,21 @@ function_list = [cs_zero,fx_hayate,fx_hayate_otsu,fx_hayate_kou,fx_hien,fx_hien_
 for element in function_list:
     stock_checker(element())
 
-print("\n\n\nMousepads that are in stock:")
-for item in in_stock_list:
+print("\n\n\nMousepads that are in stock and can be added to cart:")
+for item in in_cart_list:
     print(item)
-    if item == in_stock_list[-1]:
-        input()
         
-if len(in_stock_list) < 1:
+if len(in_cart_list) < 1:
     print("Nothing is in stock")
-    input()
+
+if len(only_stock_list) > 0:
+    print("\n\n\nMousepads that are in stock and cannot be added to cart:")
+    for item in only_stock_list:
+        print(item)
+
+if len(only_cart_list) > 0:
+    print("\n\n\nMousepads that are not in stock and can be added to cart:")
+    for item in only_cart_list:
+        print(item)
+
+input()
