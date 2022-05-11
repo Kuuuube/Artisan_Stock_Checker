6cda8e321150d7f8b98e100591f8b3b1e16b5b81
kuuube <hexagonisalie@gmail.com>
2022-05-11 10:27:30 -0400

    Refactor: Split mousepad info from main script

---
+++ artisan_mousepads.py
@@ -0,0 +1,117 @@
+#when adding a new mousepad model it must be added into the models dictionary
+#new pads will likely fit existing hardness, size, and colors so those may not need to be edited
+def mousepad_models():
+    dict_mousepad_models = {
+        "1" : "CS Zero",
+        "11" : "FX Hayate",
+        "12" : "FX Hayate Otsu",
+        "13" : "FX Hayate Kou",
+        "14" : "FX Hien",
+        "15" : "FX Hien VE",
+        "16" : "FX Zero",
+        "17" : "FX Raiden",
+        "18" : "FX Shidenkai"
+    }
+    return dict_mousepad_models
+
+def mousepad_hardnesses():
+    dict_hardnesses = {
+        "0" : "XSoft",
+        "1" : "Soft",
+        "2" : "Mid",
+        "3" : "XSoft",
+        "4" : "Soft"
+    }
+    return dict_hardnesses
+
+def mousepad_sizes():
+    dict_sizes = {
+        "1" : "S",
+        "2" : "M",
+        "3" : "L",
+        "4" : "XL"
+    }
+    return dict_sizes
+
+def mousepad_colors():
+    dict_colors = {
+        "1" : "Red",
+        "2" : "Blue",
+        "3" : "Black",
+        "5" : "Black",
+        "6" : "White",
+        "7" : "Pink",
+        "8" : "Brown"
+    }
+    return dict_colors
+
+#functions for returning each pad's specifics
+#refer to the dictionaries when editing these
+def cs_zero():
+    models = ["1"]
+    hardnesses = ["3","4","2"]
+    sizes = ["1","2","3","4"]
+    colors = ["5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_hayate():
+    models = ["11"]
+    hardnesses = ["0","1","2"]
+    sizes = ["2","3","4"]
+    colors = ["5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_hayate_otsu():
+    models = ["12"]
+    hardnesses = ["0","1","2"]
+    sizes = ["2","3","4"]
+    colors = ["1","5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_hayate_kou():
+    models = ["13"]
+    hardnesses = ["0","1","2"]
+    sizes = ["2","3","4"]
+    colors = ["3"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_hien():
+    models = ["14"]
+    hardnesses = ["0","1","2"]
+    sizes = ["1","2","3","4"]
+    colors = ["1","5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_hien_ve():
+    models = ["15"]
+    hardnesses = ["0"]
+    sizes = ["1","2","3"]
+    colors = ["1","2","5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_zero():
+    models = ["16"]
+    hardnesses = ["0","1","2"]
+    sizes = ["1","2","3","4"]
+    colors = ["5"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_raiden():
+    models = ["17"]
+    hardnesses = ["0","2"]
+    sizes = ["2","3","4"]
+    colors = ["8"]
+    return [models,hardnesses,sizes,colors]
+
+def fx_shidenkai():
+    models = ["18"]
+    hardnesses = ["0","2"]
+    sizes = ["1","2","3","4"]
+    colors = ["3","6","7"]
+    return [models,hardnesses,sizes,colors]
+
+#list of all items to check stock for
+#to skip checking items they can be removed from this list
+def active_functions():
+    function_list = [cs_zero,fx_hayate,fx_hayate_otsu,fx_hayate_kou,fx_hien,fx_hien_ve,fx_zero,fx_raiden,fx_shidenkai]
+    return function_list
--- check_artisan_stock.py
+++ check_artisan_stock.py
@@ -3,45 +3,18 @@ import re
 import itertools
 import time
 from datetime import datetime,timezone
+import artisan_mousepads
 
 request_url = "https://www.artisan-jp.com/get_syouhin.php"
 cart_url = "https://www.artisan-jp.com/stock_recheck.php"
 
-#when adding a new mousepad model it must be added into the models dictionary
-#new pads will likely fit existing hardness, size, and colors so those may not need to be edited
-dict_mousepad_models = {
-    "1" : "CS Zero",
-    "11" : "FX Hayate",
-    "12" : "FX Hayate Otsu",
-    "13" : "FX Hayate Kou",
-    "14" : "FX Hien",
-    "15" : "FX Hien VE",
-    "16" : "FX Zero",
-    "17" : "FX Raiden",
-    "18" : "FX Shidenkai"
-}
-dict_hardnesses = {
-    "0" : "XSoft",
-    "1" : "Soft",
-    "2" : "Mid",
-    "3" : "XSoft",
-    "4" : "Soft"
-}
-dict_sizes = {
-    "1" : "S",
-    "2" : "M",
-    "3" : "L",
-    "4" : "XL"
-}
-dict_colors = {
-    "1" : "Red",
-    "2" : "Blue",
-    "3" : "Black",
-    "5" : "Black",
-    "6" : "White",
-    "7" : "Pink",
-    "8" : "Brown"
-}
+dict_mousepad_models = artisan_mousepads.mousepad_models()
+
+dict_hardnesses = artisan_mousepads.mousepad_hardnesses()
+
+dict_sizes = artisan_mousepads.mousepad_sizes()
+
+dict_colors = artisan_mousepads.mousepad_colors()
 
 utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
 
@@ -115,74 +88,7 @@ def stock_checker(request_data):
         #1 second delay to be polite
         time.sleep(1)
 
-#functions for returning each pad's specifics
-#refer to the dictionaries when editing these
-def cs_zero():
-    models = ["1"]
-    hardnesses = ["3","4","2"]
-    sizes = ["1","2","3","4"]
-    colors = ["5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_hayate():
-    models = ["11"]
-    hardnesses = ["0","1","2"]
-    sizes = ["2","3","4"]
-    colors = ["5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_hayate_otsu():
-    models = ["12"]
-    hardnesses = ["0","1","2"]
-    sizes = ["2","3","4"]
-    colors = ["1","5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_hayate_kou():
-    models = ["13"]
-    hardnesses = ["0","1","2"]
-    sizes = ["2","3","4"]
-    colors = ["3"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_hien():
-    models = ["14"]
-    hardnesses = ["0","1","2"]
-    sizes = ["1","2","3","4"]
-    colors = ["1","5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_hien_ve():
-    models = ["15"]
-    hardnesses = ["0"]
-    sizes = ["1","2","3"]
-    colors = ["1","2","5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_zero():
-    models = ["16"]
-    hardnesses = ["0","1","2"]
-    sizes = ["1","2","3","4"]
-    colors = ["5"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_raiden():
-    models = ["17"]
-    hardnesses = ["0","2"]
-    sizes = ["2","3","4"]
-    colors = ["8"]
-    return [models,hardnesses,sizes,colors]
-
-def fx_shidenkai():
-    models = ["18"]
-    hardnesses = ["0","2"]
-    sizes = ["1","2","3","4"]
-    colors = ["3","6","7"]
-    return [models,hardnesses,sizes,colors]
-
-#list of all items to check stock for
-#to skip checking items they can be removed from this list
-function_list = [cs_zero,fx_hayate,fx_hayate_otsu,fx_hayate_kou,fx_hien,fx_hien_ve,fx_zero,fx_raiden,fx_shidenkai]
+function_list = artisan_mousepads.active_functions()
 
 for element in function_list:
     stock_checker(element())
