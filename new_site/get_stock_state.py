5f0329e1d48b30dee1ca34b854bddfb07efd147a
kuuuube <hexagonisalie@gmail.com>
2025-08-07 11:12:54 -0400

    Add support for single sku products

--- new_site/get_stock_state.py
+++ new_site/get_stock_state.py
@@ -1,21 +1,48 @@
 import requests
 import re
 import json
+import logger
 
 
-def get_raw_stock_data():
-    artisan_fx_url = "https://artisan-jp.com/global/products/ninja-fx-series.html"
+artisan_fx_url = "https://artisan-jp.com/global/products/ninja-fx-series.html"
+artisan_classic_url = "https://artisan-jp.com/global/products/classic-series.html"
+artisan_accessories_url = "https://artisan-jp.com/global/products/accessories.html"
+
+def get_stock_data(url):
     artisan_cert_path = "www-artisan-jp-com.pem" # shim due to requests not recognizing `GlobalSign nv-sa` cert
-    response = requests.get(artisan_fx_url, verify = artisan_cert_path)
+    response = requests.get(url, verify = artisan_cert_path)
 
     response_text_stripped = response.text.replace("\n", "").replace("\r", "")
     product_containers = re.findall('<li class="item product product-item">.*?</li>', response_text_stripped)
-    raw_stock_data_jsons = []
+    magento_swatch_stock_data_jsons = []
+    single_sku_stock_data_dicts = []
     for product_container in product_containers:
         magento_jsons = re.findall('(?<=<script type="text/x-magento-init">).*?(?=</script>)', product_container)
         product_link_container = re.search('<a class="product-item-link".*?</a>', product_container)[0]
         product_name = re.search('(?<=>).*?(?=<)', product_link_container)[0].strip()
         product_link = re.search('(?<=href=").*?(?=")', product_link_container)[0].strip()
+
+        # product with no selectable options, single sku
+        if (len(magento_jsons) == 0):
+            in_stock = re.search('Add( |&#x20;)to( |&#x20;)Cart', product_container)
+            out_of_stock = re.search('(Out of stock|stock unavailable)', product_container)
+            if (bool(in_stock) == bool(out_of_stock)):
+                logger.log("Single sku in stock and out of stock match, in_stock: " + str(in_stock) + ", out_of_stock: " + str(out_of_stock))
+
+            sku_search = re.search('(?<=data-product-sku=").*?(?=")', product_container)
+            if (not sku_search):
+                logger.log("Single sku item failed to find sku, product_container: " + product_container)
+                continue
+
+            stock_data = {
+                "product_name": product_name,
+                "product_link": product_link,
+                "sku": sku_search[0],
+                "in_stock": bool(in_stock),
+            }
+
+            single_sku_stock_data_dicts.append(stock_data)
+
         for magento_json in magento_jsons:
             magento_json_parsed = json.loads(magento_json)
             for key in magento_json_parsed.keys():
@@ -23,16 +50,16 @@ def get_raw_stock_data():
                     truncated_magento_json = magento_json_parsed[key]["Magento_Swatches/js/swatch-renderer"]
                     truncated_magento_json["product_name"] = product_name
                     truncated_magento_json["product_link"] = product_link
-                    raw_stock_data_jsons.append(truncated_magento_json)
+                    magento_swatch_stock_data_jsons.append(truncated_magento_json)
 
-    return raw_stock_data_jsons
+    return {"magento_swatch_stock_data": magento_swatch_stock_data_jsons, "single_sku_stock_data_dicts": single_sku_stock_data_dicts}
 
-def get_product_info():
+def get_product_info(url):
     product_info_dict = {}
 
-    raw_stock_data_jsons = get_raw_stock_data()
-    for raw_stock_data_json in raw_stock_data_jsons:
-        attributes = raw_stock_data_json["jsonConfig"]["attributes"]
+    stock_data_jsons = get_stock_data(url)
+    for magento_swatch_stock_data_json in stock_data_jsons["magento_swatch_stock_data"]:
+        attributes = magento_swatch_stock_data_json["jsonConfig"]["attributes"]
         for attribute_value in attributes.values():
             attribute_code = attribute_value["code"] # name of attribute
             for option in attribute_value["options"]:
@@ -51,21 +78,24 @@ def get_product_info():
                             "price": 0,
                         }
 
-        salable = raw_stock_data_json["jsonConfig"]["salable"]
+        salable = magento_swatch_stock_data_json["jsonConfig"]["salable"]
         for salable_value in salable.values():
             for product_ids in salable_value.values():
                 for product_id in product_ids:
                     if product_id in product_info_dict:
                         product_info_dict[product_id]["in_stock"] = True
 
-        skus = raw_stock_data_json["jsonConfig"]["sku"]
+        skus = magento_swatch_stock_data_json["jsonConfig"]["sku"]
         for product_id, sku in skus.items():
             if product_id in product_info_dict:
                 product_info_dict[product_id]["sku"] = sku
 
-        option_prices = raw_stock_data_json["jsonConfig"]["optionPrices"]
+        option_prices = magento_swatch_stock_data_json["jsonConfig"]["optionPrices"]
         for product_id, prices in option_prices.items():
             if product_id in product_info_dict:
                 product_info_dict[product_id]["price"] = prices["finalPrice"]["amount"]
+
+    for single_sku_stock_data_dict in stock_data_jsons["single_sku_stock_data_dicts"]:
+        product_info_dict[single_sku_stock_data_dict["sku"]] = single_sku_stock_data_dict
 
     return product_info_dict
