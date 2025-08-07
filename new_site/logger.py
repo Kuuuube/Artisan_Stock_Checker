230ef8b01b63184aa9c0b12a906da91991645450
kuuuube <hexagonisalie@gmail.com>
2025-08-07 11:10:58 -0400

    Only print to log as last resort

--- new_site/logger.py
+++ new_site/logger.py
@@ -6,8 +6,6 @@ def error_log(message, error = ""):
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
         with open ("error_log.txt", "a", encoding="utf8") as log_file:
             log_file.write(utc_time + ", " + str(message).replace("\r", r"\r").replace("\n", r"\n") + ", " + str(error).replace("\r", r"\r").replace("\n", r"\n") + "\n")
-        print(message)
-        print(error)
     except Exception:
         try_print("Could not write to error log:")
         try_print(traceback.format_exc())
@@ -17,7 +15,6 @@ def log(message):
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
         with open ("log.txt", "a", encoding="utf8") as log_file:
             log_file.write(utc_time + ", " + str(message).replace("\r", r"\r").replace("\n", r"\n") + "\n")
-        print(message)
     except Exception:
         try_print("Could not write to log:")
         try_print(traceback.format_exc())
