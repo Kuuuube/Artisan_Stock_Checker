eeffa463ac51e7ee8d6db6a8bdcbb85031ff84d1
kuuuube <hexagonisalie@gmail.com>
2025-08-08 00:13:08 -0400

    Add artisan_all_products_url and set to 25 products per page

--- new_site/check_artisan_stock_webhook.py
+++ new_site/check_artisan_stock_webhook.py
@@ -11,9 +11,12 @@ import stock_state_handler
 
 utc_time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
 
-artisan_fx_url = "https://artisan-jp.com/global/products/ninja-fx-series.html"
-artisan_classic_url = "https://artisan-jp.com/global/products/classic-series.html"
-artisan_accessories_url = "https://artisan-jp.com/global/products/accessories.html"
+# all of these pages max out at 25 products
+# if artisan ever has more products, scraping multiple pages will need to be added (not skus, product variations dont count)
+artisan_all_products_url = "https://artisan-jp.com/global/products.html?product_list_limit=25"
+artisan_fx_url = "https://artisan-jp.com/global/products/ninja-fx-series.html?product_list_limit=25"
+artisan_classic_url = "https://artisan-jp.com/global/products/classic-series.html?product_list_limit=25"
+artisan_accessories_url = "https://artisan-jp.com/global/products/accessories.html?product_list_limit=25"
 
 CONFIG_DIR = os.environ.get("ARTISAN_STOCK_CHECKER_CONFIG_DIR", ".")
 if CONFIG_DIR and os.path.exists(CONFIG_DIR):
@@ -41,7 +44,7 @@ while True:
     try:
         current_epoch_time_ms_str = str(int(datetime.datetime.now(tz = datetime.timezone.utc).timestamp() * 1000))
 
-        full_stock_data = stock_checker.get_stock_data(artisan_fx_url, request_fail_delay)
+        full_stock_data = stock_checker.get_stock_data(artisan_all_products_url, request_fail_delay)
         safe_write_stock_json("full_stock_data_" + current_epoch_time_ms_str, full_stock_data)
 
         product_infos = stock_checker.parse_stock_data(full_stock_data)
